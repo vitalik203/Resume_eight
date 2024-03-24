65c22fbc1745517fd69fe2091aafefc7c750cde8
VitaliiZaluskii <vzaluskiy23@gmail.com>
2024-03-24 12:17:50 +0200

    fix: initial commit

--- release.config.js
+++ release.config.js
@@ -4,14 +4,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    {
-      // Custom configuration for @semantic-release/git plugin
-      assets: ["CHANGELOG.md"],
-      message:
-        "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    },
-    "@semantic-release/github",
     "@semantic-release/npm",
+    "@semantic-release/github",
   ],
 };
